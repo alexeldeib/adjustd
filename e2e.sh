a2ba4469f4f7b843221d36ac4658650a77bc48da
Alexander Eldeib <alexeldeib@gmail.com>
2020-05-29 03:04:06 -0700

    try to exec binary in vm in pipeline

--- e2e.sh
+++ e2e.sh
@@ -15,5 +15,9 @@ setup
 boot
 wait_for_boot
 sync_file target/debug/adjustd /home/ubuntu/adjustd
+sync_file data/config.yaml /home/ubuntu/config.yaml
 vm_ssh sudo mv /home/ubuntu/adjustd /usr/local/bin/adjustd
+vm_ssh sudo mkdir p /usr/local/bin/data
+vm_ssh sudo mv /home/ubuntu/config.yaml /usr/local/bin/data/config.yaml
+vm_ssh sudo adjustd
 vm_ssh du -b /usr/local/bin/adjustd | numfmt --to=iec
